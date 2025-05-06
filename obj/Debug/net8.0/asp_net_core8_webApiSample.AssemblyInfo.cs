5acc85adff5dcaad48bc52cb5fcc6fc6f4d569f9
test <Brian@asegobal.com>
2025-05-06 21:08:48 +0800

    test

--- obj/Debug/net8.0/asp_net_core8_webApiSample.AssemblyInfo.cs
+++ obj/Debug/net8.0/asp_net_core8_webApiSample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("asp_net_core8_webApiSample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcd62ca0f35c4abb72204c8e95b1fa423b41c933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24f99b880b7bd1e8cdad047bcb5ba7c18ff15046")]
 [assembly: System.Reflection.AssemblyProductAttribute("asp_net_core8_webApiSample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("asp_net_core8_webApiSample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
